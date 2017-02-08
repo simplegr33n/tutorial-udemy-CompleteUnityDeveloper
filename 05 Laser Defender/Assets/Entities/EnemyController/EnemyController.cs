8868829c156871197bf1e1bbf5c5d8baf819689f
simplegr33n <gcgolda@gmail.com>
2017-02-07 18:16:05 -0800

    refactoring

---
+++ 05 Laser Defender/Assets/Entities/EnemyController/EnemyController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+
+    public float minX;
+    public float maxX;
+
+    public float width = 10f;
+    public float height = 5f;
+
+    public float speed = 5f;
+
+    private bool moveRight = true;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        foreach (Transform child in transform)
+        {
+            GameObject enemy = Instantiate(enemyPrefab, child.transform.position, Quaternion.identity) as GameObject;
+            enemy.transform.parent = child;
+        }
+
+    }
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector3(width, height));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (transform.position.x >= maxX)
+        {
+            moveRight = false;
+        }
+
+        if (transform.position.x <= minX)
+        {
+            moveRight = true;
+        }
+
+        if (moveRight)
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+
+        if (!moveRight)
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+
+
+    }
+}
